131bf54696c98bbf98cb5ba3620330c133d1c375
Ragnov <vicente35713@gmail.com>
2022-01-20 16:30:50 +0800

    add css styles and implement inline style

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import "./index.css";
 
 // My first component
 function App() {
+	const paraStyle = {
+		"marginBottom":"15px"
+	}
   return (
   	<div className="app">
   	<header className="app-header">
   	<h1>My Amazing ToDo-List App</h1>
-  	<p>The most simple and Amazing to do list app</p>
+  	<p style = {paraStyle} >The most simple and Amazing to do list app</p>
+  	<ul>
+  		<li className="tasks-item">Sleep later</li>
+  		<li className="tasks-item">Feed the cat at 6pm</li>
+  		<li className="tasks-item">watch the new episode of one piece</li>
+  	</ul>
   	</header>
   	</div>
   	);
