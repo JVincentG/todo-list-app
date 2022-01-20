8c23ea14cd6f8b8186cfee0a530ccbc9b0ea6d70
Ragnov <vicente35713@gmail.com>
2022-01-20 16:58:16 +0800

    Use of properties and creating task component

--- src/index.js
+++ src/index.js
@@ -3,16 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 // My first component
+
+
+function Task(props){
+	//console.log(props);
+	return(
+		<li className="tasks-item">{props.taskName}</li> //
+
+		);
+	}
 function TaskList(){
 	return(
 		<ul>
-  		<li className="tasks-item">Sleep later</li>
-  		<li className="tasks-item">Feed the cat at 6pm</li>
-  		<li className="tasks-item">watch the new episode of one piece</li>
+  		<Task taskName="Sleep"/>
+  		<Task taskName="Feed Cat"/>
+  		<Task taskName="Procrastinate"/>
   	    </ul>
 		);
 }
-
+//
 function App() {
 	const paraStyle = {
 		"marginBottom":"15px"
