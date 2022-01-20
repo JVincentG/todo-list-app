5419af3a9874ce15100d5492ff1e799b98b82900
Ragnov <vicente35713@gmail.com>
2022-01-20 16:44:02 +0800

    Create a new tasklist component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 // My first component
+function TaskList(){
+	return(
+		<ul>
+  		<li className="tasks-item">Sleep later</li>
+  		<li className="tasks-item">Feed the cat at 6pm</li>
+  		<li className="tasks-item">watch the new episode of one piece</li>
+  	    </ul>
+		);
+}
+
 function App() {
 	const paraStyle = {
 		"marginBottom":"15px"
@@ -12,11 +22,7 @@ function App() {
   	<header className="app-header">
   	<h1>My Amazing ToDo-List App</h1>
   	<p style = {paraStyle} >The most simple and Amazing to do list app</p>
-  	<ul>
-  		<li className="tasks-item">Sleep later</li>
-  		<li className="tasks-item">Feed the cat at 6pm</li>
-  		<li className="tasks-item">watch the new episode of one piece</li>
-  	</ul>
+  	<TaskList/>
   	</header>
   	</div>
   	);
